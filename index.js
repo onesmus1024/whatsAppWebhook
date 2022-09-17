cc19e085c31655538b31458dba5a8ecfa9989037
onesmus <onesmus1024@gmail.com>
2022-09-17 22:02:19 +0300

    bug fix

--- index.js
+++ index.js
@@ -40,12 +40,10 @@ app.post("/webhook",(req,res)=>{
     if(body_param.object){
         console.log("inside body param");
         if(body_param.entry && 
-            body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.messages && 
-            body_param.entry[0].changes[0].value.messages[0]  
+            body_param.entry[0].changes  
             ){
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let from = '+254710383551';//body_param.entry[0].changes[0].value.messages[0].from; 
                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
                console.log("phone number "+phon_no_id);
