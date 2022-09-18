7fd4a1b81973e1210945ee9b322cd83c4ad8040f
onesmus <onesmus1024@gmail.com>
2022-09-18 09:11:13 +0300

    bug fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app=express().use(body_parser.json());
 
 const token=process.env.TOKEN;
-const mytoken=process.env.MYTOKEN;
+const mytoken=process.env.MYTOKEN;//prasath_token
 
 app.listen(process.env.PORT,()=>{
     console.log("webhook is listening");
@@ -31,7 +31,7 @@ app.get("/webhook",(req,res)=>{
 
 });
 
-app.post("/webhook",(req,res)=>{
+app.post("/webhook",(req,res)=>{ //i want some 
 
     let body_param=req.body;
 
@@ -40,11 +40,13 @@ app.post("/webhook",(req,res)=>{
     if(body_param.object){
         console.log("inside body param");
         if(body_param.entry && 
-            body_param.entry[0].changes  
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
             ){
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-               let from = '+254710383551';//body_param.entry[0].changes[0].value.messages[0].from; 
-               let msg_body = "hi"//body_param.entry[0].changes[0].value.messages[0].text.body;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
                console.log("phone number "+phon_no_id);
                console.log("from "+from);
@@ -57,7 +59,7 @@ app.post("/webhook",(req,res)=>{
                        messaging_product:"whatsapp",
                        to:from,
                        text:{
-                           body:"Hi.. I'm Onesmus from molynew, your message is "+msg_body
+                           body:"Hi.. I'm Prasath, your message is "+msg_body
                        }
                    },
                    headers:{
@@ -68,13 +70,9 @@ app.post("/webhook",(req,res)=>{
 
                res.sendStatus(200);
             }else{
-                console.log("missing parameter 1");
                 res.sendStatus(404);
             }
 
-    }else{
-        console.log("missing parameter 2");
-        res.sendStatus(404);
     }
 
 });
